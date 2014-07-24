9dbb31d8333630dc9ceee4529712670984d68aa0
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2014-07-24 12:28:01 +0200

    keep real password apart

---
+++ rcp_parameters.php
@@ -0,0 +1,3 @@
+define( 'LS_BASEURL', 'http://domain-name/');  // adjust this one to your actual LimeSurvey URL
+define( 'LS_USER', 'user' ); 
+define( 'LS_PASSWORD', 'password' );
--- testrpcpl.php
+++ testrpcpl.php
@@ -1,45 +1,48 @@
 <?php
 
+// http://mcp.ocd-dbs-france.org/test/testrpcpl.php?answer={%22366836X99X43%22:%22A2%22,%22366836X106X105%22:%22A2%22,%22366836X101X116%22:%2254%22,%22sid%22:%22%20366836%22}
+
+if(!isset($_GET['answer']))die();
+
 require_once '../application/libraries/jsonRPCClient.php';
-define( 'LS_BASEURL', 'http://mcp.ocd-dbs-france.org/');  // adjust this one to your actual LimeSurvey URL
-define( 'LS_USER', 'rpcuser' ); //rpcusermail2@yopmail.com
-define( 'LS_PASSWORD', 'jflksb6QZwh' );
+
+
+$response_data = array();
+$jsonparameter = json_decode($_GET['answer']);
+foreach ($jsonparameter as $key => $value) {
+ $response_data[$key] = $value;
+}
 
 // the survey to process
-$survey_id=934317;
-echo $survey_id;
+$survey_id= $response_data['sid'];
+
 // instanciate a new client
 $myJSONRPCClient = new jsonRPCClient( LS_BASEURL.'/admin/remotecontrol' );
 
 // receive session key
 $sessionKey= $myJSONRPCClient->get_session_key( LS_USER, LS_PASSWORD );
 
+/*
 // receive all ids and info of groups belonging to a given survey
 $groups = $myJSONRPCClient->list_groups( $sessionKey, $survey_id );
 print_r($groups, null );
+echo "<br>";
 
-
-$q = $myJSONRPCClient->list_questions( $sessionKey, $survey_id,4 );
+$q = $myJSONRPCClient->list_questions( $sessionKey, $survey_id,99 );
 print_r($q, null );
+echo "<br>";
+*/
 
 //http://stackoverflow.com/questions/23121669/limesurvey-remotecontrol2-api-any-add-response-php-examples
 //get response data from FORM input values
-$response_data = array(); 
-//foreach ($_POST as $key => $value) {
-//  $response_data[$key] = $value;
-//}  
-$response_data['934317X4X10'] = 'A2';
-$response_data['934317X5X11'] = '44';
-$response_data['934317X6X12'] = '3';
-$response_data['934317X6X13'] = '';
-$response_data['934317X6X14'] = 'A1';
-
-$response_data['sid'] = '934317';
 
 
 $responseadded = $myJSONRPCClient->add_response($sessionKey, $survey_id, $response_data);
 // show the reponse code
-print_r($responseadded);
+if(is_int($responseadded))
+    echo "1"; // $responseadded value is the id of recorded answer
+    else
+    echo "0"; // array with error explaination
 
 // release the session key
 $myJSONRPCClient->release_session_key( $sessionKey );
